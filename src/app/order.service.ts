c6b342bffe89bb55f4d7c0e571f72e727149a5cd
abhilash <abhilash.kamble376@gmail.com>
2021-02-02 22:40:52 +0530

    order

---
+++ src/app/order.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+
+  url=`http://localhost:3000`
+
+  constructor(private httpClient: HttpClient) { }
+
+  getOrders() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        token: sessionStorage['token']
+      })
+    };
+
+    return this.httpClient.get(this.url+'/myorders', httpOptions)
+
+  }
+}
--- src/app/order/order-list/order-list.component.ts
+++ src/app/order/order-list/order-list.component.ts
@@ -1,3 +1,4 @@
+import { OrderService } from './../../order.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,24 +9,37 @@ import { Component, OnInit } from '@angular/core';
 export class OrderListComponent implements OnInit {
 
 orders=[
-  {"Order No":1,"Customer Name":"steve jobs","Offer":"MANSOON DHAMAKA","Discount":20,"Vehicle No":"MH12LH2951","Amount":3000,"active":1},
-  {"Order No":2,"Customer Name":"steve jobs","Offer":"MANSOON DHAMAKA","Discount":20,"Vehicle No":"MH12LH2951","Amount":3000,"active":0},
-  {"Order No":3,"Customer Name":"steve jobs","Offer":"MANSOON DHAMAKA","Discount":20,"Vehicle No":"MH12LH2951","Amount":3000,"active":1},
-  {"Order No":4,"Customer Name":"steve jobs","Offer":"MANSOON DHAMAKA","Discount":20,"Vehicle No":"MH12LH2951","Amount":3000,"active":1},
-  {"Order No":5,"Customer Name":"steve jobs","Offer":"MANSOON DHAMAKA","Discount":20,"Vehicle No":"MH12LH2951","Amount":3000,"active":1},
-  {"Order No":6,"Customer Name":"steve jobs","Offer":"MANSOON DHAMAKA","Discount":20,"Vehicle No":"MH12LH2951","Amount":3000,"active":0},
-  {"Order No":7,"Customer Name":"steve jobs","Offer":"MANSOON DHAMAKA","Discount":20,"Vehicle No":"MH12LH2951","Amount":3000,"active":1},
-  {"Order No":7,"Customer Name":"steve jobs","Offer":"MANSOON DHAMAKA","Discount":20,"Vehicle No":"MH12LH2951","Amount":3000,"active":0},
-  {"Order No":8,"Customer Name":"steve jobs","Offer":"MANSOON DHAMAKA","Discount":20,"Vehicle No":"MH12LH2951","Amount":3000,"active":1}
+  // {"Order No":1,"Customer Name":"steve jobs","Offer":"MANSOON DHAMAKA","Discount":20,"Vehicle No":"MH12LH2951","Amount":3000,"active":1},
+  // {"Order No":2,"Customer Name":"steve jobs","Offer":"MANSOON DHAMAKA","Discount":20,"Vehicle No":"MH12LH2951","Amount":3000,"active":0},
+  // {"Order No":3,"Customer Name":"steve jobs","Offer":"MANSOON DHAMAKA","Discount":20,"Vehicle No":"MH12LH2951","Amount":3000,"active":1},
+  // {"Order No":4,"Customer Name":"steve jobs","Offer":"MANSOON DHAMAKA","Discount":20,"Vehicle No":"MH12LH2951","Amount":3000,"active":1},
+  // {"Order No":5,"Customer Name":"steve jobs","Offer":"MANSOON DHAMAKA","Discount":20,"Vehicle No":"MH12LH2951","Amount":3000,"active":1},
+  // {"Order No":6,"Customer Name":"steve jobs","Offer":"MANSOON DHAMAKA","Discount":20,"Vehicle No":"MH12LH2951","Amount":3000,"active":0},
+  // {"Order No":7,"Customer Name":"steve jobs","Offer":"MANSOON DHAMAKA","Discount":20,"Vehicle No":"MH12LH2951","Amount":3000,"active":1},
+  // {"Order No":7,"Customer Name":"steve jobs","Offer":"MANSOON DHAMAKA","Discount":20,"Vehicle No":"MH12LH2951","Amount":3000,"active":0},
+  // {"Order No":8,"Customer Name":"steve jobs","Offer":"MANSOON DHAMAKA","Discount":20,"Vehicle No":"MH12LH2951","Amount":3000,"active":1}
 ]
 
-  constructor() { }
+  constructor(private orderService:OrderService) { }
 
   ngOnInit(): void {
     this.loadOrders()
   }
 
-  loadOrders(){}
+ 
+
+  loadOrders() {
+    this.orderService
+      .getOrders()
+      .subscribe(response => {
+        if (response['status'] == 'success') {
+          this.orders = response['data']
+          console.log(this.orders)
+        } else {
+          console.log(response['error'])
+        }
+      })
+  }
 
   toggleActive(order) {
   
